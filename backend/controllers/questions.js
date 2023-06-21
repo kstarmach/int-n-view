df49e76543acf17c57473aba5a5fe53c121ccc65
Kamil Starmach <kstarmach@outlook.com>
2023-06-21 14:57:48 +0200

    Add single GET request

--- backend/controllers/questions.js
+++ backend/controllers/questions.js
@@ -6,8 +6,20 @@ questionsRouter.post('/', async (req, res) => {
         const { question, answer } = req.body;
 
         const newQuestion = new Question({
-            question,
-            answer
+            question: {
+                type: String,
+                required: true
+            },
+            answer: {
+                type: String,
+                required: true
+            },
+            created_at: {
+                type: Date
+            },
+            update_at: {
+                type: Date
+            }
         })
 
         const savedQuestion = await newQuestion.save();
@@ -26,6 +38,18 @@ questionsRouter.get('/', async (req, res) => {
     } catch (error) {
         console.error('Error retrieving items:', error);
         res.status(404).json({ error: 'Something went wrong with GET!' })
+    }
+})
+
+questionsRouter.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const singleQuestion = await Question.findById(id);
+
+        res.json(singleQuestion);
+    } catch (error) {
+        console.error('Error retrieving items:', error);
+        res.status(404).json({ error: 'Something went wrong with SINGLE GET!' })
     }
 })
 
